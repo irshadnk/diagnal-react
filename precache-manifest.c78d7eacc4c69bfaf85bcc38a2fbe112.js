35e6dae46a53cfa851a526cedcd08bde63397712
Irshad <irshadnu@gmail.com>
2018-12-27 00:15:22 +0530

    Updates

---
+++ precache-manifest.c78d7eacc4c69bfaf85bcc38a2fbe112.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/diagnal-react/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "51fcef9c8179f25a8d8e",
+    "url": "/diagnal-react/static/css/main.b8aca2ba.chunk.css"
+  },
+  {
+    "revision": "869d986ac0a8d157cac8",
+    "url": "/diagnal-react/static/js/1.869d986a.chunk.js"
+  },
+  {
+    "revision": "50b48f5680de933af55f",
+    "url": "/diagnal-react/static/js/runtime~main.50b48f56.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/diagnal-react/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/diagnal-react/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/diagnal-react/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "51fcef9c8179f25a8d8e",
+    "url": "/diagnal-react/static/js/main.51fcef9c.chunk.js"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/diagnal-react/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/diagnal-react/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/diagnal-react/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/diagnal-react/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/diagnal-react/static/media/poster9.65d6d500.jpg"
+  },
+  {
+    "revision": "34263b790afcc1572d8c8d762604c087",
+    "url": "/diagnal-react/index.html"
+  }
+];
